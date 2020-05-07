126adfd3a7dd0ecda3bdfbda5808e0c784c28e7c
chen.jiang <chen.jiang@qiaofang.cn>
2020-05-08 01:12:13 +0800

    二叉树的前序遍历 递归法

--- src/easy/ReverseList.java
+++ src/easy/ReverseList.java
@@ -1,6 +1,6 @@
 package easy;
 
-import base.ListNode;
+import struct.ListNode;
 
 /**
  * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
---
+++ src/struct/TreeNode.java
@@ -0,0 +1,15 @@
+package struct;
+
+/**
+ * @author JiangChen
+ * @date 2020/05/08
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
