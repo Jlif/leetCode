0d1ca0d00408075fa658a0ba50573275ac9303d9
jiangchen <cjiang@86links.com>
2020-05-12 19:58:06 +0800

    5-12 z最小栈

---
+++ src/easy/MinStack.java
@@ -0,0 +1,67 @@
+package easy;
+
+import java.util.Stack;
+
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) —— 将元素 x 推入栈中。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入：
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * <p>
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ * <p>
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ *  
+ * <p>
+ * 提示：
+ * pop、top 和 getMin 操作总是在 非空栈 上调用。
+ */
+public class MinStack {
+
+    private Stack<Integer> stack = new Stack<>();
+
+    public void push(int x) {
+        stack.push(x);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return stack.stream().min(Integer::compareTo).orElse(0);
+    }
+
+    public static void main(String[] args) {
+        MinStack stack = new MinStack();
+        stack.push(-1);
+        stack.push(3);
+        stack.push(9);
+        stack.push(2);
+        stack.pop();
+        System.out.println(stack.top());
+        System.out.println(stack.getMin());
+    }
+}
