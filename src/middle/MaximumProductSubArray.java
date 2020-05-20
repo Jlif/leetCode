16996fcaacd00a37fae03fc24d38f1ceb38a12b6
jiangchen <cjiang@86links.com>
2020-05-20 20:18:45 +0800

    5-20

---
+++ src/middle/MaximumProductSubArray.java
@@ -0,0 +1,16 @@
+package middle;
+
+/**
+ * @author jiangchen
+ */
+public class MaximumProductSubArray {
+    public static int maxProduct(int[] nums) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, -2, 6, 9, -1};
+        System.out.println(maxProduct(arr));
+    }
+}
