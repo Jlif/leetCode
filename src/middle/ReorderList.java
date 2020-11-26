618d14b84a08491a08fda529c8e84b1d73dff0d8
jlif <jiangchen23@qq.com>
2020-11-27 00:55:17 +0800

    提交

--- src/middle/ReorderList.java
+++ src/middle/ReorderList.java
@@ -2,6 +2,9 @@ package middle;
 
 import struct.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ， 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
  *
@@ -21,21 +24,33 @@ import struct.ListNode;
  */
 public class ReorderList {
 
-    public static void reorderList(ListNode head) {
-        ListNode begin = head;
-        ListNode end = head;
-        while (end.next != null) {
-            end = end.next;
-        }
-        while (head.next != null || end.next != null) {
+    public static ListNode reorderList(ListNode head) {
+        List<ListNode> arr = new ArrayList<>();
 
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        arr.add(new ListNode(head.val));
+        while (head.next != null) {
+            arr.add(new ListNode(head.next.val));
+            head = head.next;
         }
+        int i;
+        for (i = 0; i < arr.size() - i - 1; i++) {
+            temp.next = arr.get(i);
+            temp = temp.next;
+            temp.next = arr.get(arr.size() - i - 1);
+            temp = temp.next;
+        }
+        System.out.println(i);
 
+        if (arr.size() - i == i) {
+            temp.val = arr.get(i).val;
+        }
+        return dummy.next;
     }
 
     public static void main(String[] args) {
         ListNode list = ListNode.initWithParams(1, 2, 3, 4, 5, 6, 7);
-        reorderList(list);
-        System.out.println(list);
+        System.out.println(reorderList(list));
     }
 }
